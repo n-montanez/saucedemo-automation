34dcf525ab7da14e469be47b9d99fb041c4810cc
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-03-12 09:58:06 -0500

    fix: small code smells

--- src/test/java/com/globant/tests/LogoutTest.java
+++ src/test/java/com/globant/tests/LogoutTest.java
@@ -1,11 +1,9 @@
 package com.globant.tests;
 
 import com.globant.pages.Header;
-import com.globant.pages.LoginPage;
 import com.globant.pages.ProductsPage;
 import com.globant.pages.SidebarMenu;
 import com.globant.utils.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -21,6 +19,6 @@ public class LogoutTest extends BaseTest {
     public void Logout() {
         Header header = new Header(productsPage.getDriver());
         SidebarMenu sidebarMenu = header.openBurgerMenu();
-        sidebarMenu.logout();
+        LoginPage loginPage = sidebarMenu.logout();
     }
 }
--- src/test/java/com/globant/tests/RemoveItemsFromCartTest.java
+++ src/test/java/com/globant/tests/RemoveItemsFromCartTest.java
@@ -7,8 +7,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 public class RemoveItemsFromCartTest extends BaseTest {
     private ProductsPage productsPage;
 
